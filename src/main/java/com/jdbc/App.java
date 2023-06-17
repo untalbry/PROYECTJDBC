eda856ddc7f28dba88ae61cecedf9e5f8b8b4243
Bryan Castro <91161604+xVrzBx@users.noreply.github.com>
2023-06-17 16:00:14 -0600

    starting db proyect

---
+++ src/main/java/com/jdbc/App.java
@@ -0,0 +1,10 @@
+package com.jdbc;
+
+import com.jdbc.view.View;
+
+public class App {
+    public static void main(String[] args) {
+        View view = new View();
+        view.setVisible(true);
+    }
+}
---
+++ src/main/java/com/jdbc/controller/Controller.java
@@ -0,0 +1,5 @@
+package com.jdbc.controller;
+
+public class Controller {
+
+}
